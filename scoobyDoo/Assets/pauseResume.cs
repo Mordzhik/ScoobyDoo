51cab311409607c18817d280c0d889dcc3c9ad13
Marzhan <nurl-imarzhan@mail.ru>
2021-02-08 18:26:00 +0600

    first commit

---
+++ scoobyDoo/Assets/pauseResume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseResume : MonoBehaviour {
+
+	public bool pause;
+	// Use this for initialization
+	void Start () {
+		pause = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+	public void pauseandResume(){
+		pause = !pause;
+		if (!pause) {
+			Time.timeScale = 1;
+		} else {
+			Time.timeScale = 0;
+		}
+	}
+}
